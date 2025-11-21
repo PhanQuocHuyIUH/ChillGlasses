565e30f056996abe35ac4af60b46b19e16f61aea
tranh <ntrmanh71@gmail.com>
2025-11-21 22:43:10 +0700

    feat: implement home page ui

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
-  // images: {
-  //   domains: ["localhost", "your-backend-domain.com"],
-  // },
-  // env: {
-  //   NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-  // },
+  images: {
+    domains: ["localhost", "your-backend-domain.com", "kinhmateyeplus.com", "via.placeholder.com"], // Thêm các domain được phép
+  },
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
 };
 
 export default nextConfig;
