8192800ee202a8b6598009b2253b5f7a39906884
tranh <ntrmanh71@gmail.com>
2025-11-21 21:07:46 +0700

    feat: implement public layout with header and footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import { cn } from "@/lib/utils";
+import Header from "../components/layout/Header";
+import Footer from "../components/layout/Footer";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center font-sans bg-amber-50">
+      <Header/>
       <h1 className={cn("text-3xl", "text-red-500")}>Home page here</h1>
+      <Footer/>
     </div>
   );
 }
