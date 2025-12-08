4ade2b3ac7573c35469e41faeab54f79f9411c2b
tranh <ntrmanh71@gmail.com>
2025-12-08 21:11:52 +0700

    feat: update ui and product detail

--- src/app/(customer)/products/gong-kinh-can/page.tsx
+++ src/app/(customer)/products/gong-kinh-can/page.tsx
@@ -42,14 +42,14 @@ export default function GongKinhCanPage() {
 
   return (
     <div className="w-full max-w-6xl mx-auto py-8 text-black">
-      <h1 className="text-3xl font-bold mb-6 text-center">Gọng Kính Cận</h1>
+      <h1 className="text-3xl font-bold mb-6 text-center">GỌNG KÍNH CẬN</h1>
 
       <div className="grid grid-cols-2 md:grid-cols-4 gap-6">
         {products.slice(0, visibleProducts).map((product) => (
           <Link
             key={product.id}
             href={`/products/${product.id}`}
-            className="border p-4 rounded shadow hover:shadow-lg"
+            className="border p-4 hover:-translate-y-1 rounded shadow hover:shadow-lg"
           >
             <Image
               src={product.primaryImageUrl}
